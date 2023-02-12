157dc9a1f2838fe14e8e7891923cfbdc29959b7a
priyanshu1430 <priyanshugpt143@gmail.com>
2023-02-12 22:32:47 +0530

    done

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app =express();
+const mongoose = require('mongoose');
+
+
+
+
+
+app.listen(4000, ()=>{
+    console.log("Backend Server is running")
+})
