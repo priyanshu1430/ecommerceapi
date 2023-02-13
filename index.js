983d254318e2409f56482c50a86ef7163a329b96
priyanshu1430 <priyanshugpt143@gmail.com>
2023-02-13 22:20:17 +0530

    d

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app =express();
 const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+const userRoute = require("./routes/user")
+const authRoute = require('./routes/auth')
 
 
+dotenv.config();
+
+try{
+    mongoose.set("strictQuery", false);
+    mongoose.connect(process.env.MONGO_Url);
+    console.log("DB Connection Successful");
+}
+catch(err){
+    console.log(err)
+}
+
+app.use(express.json());
+
+app.use("/api/auth",authRoute)
+app.use("/api/users",userRoute)
 
 
-
-app.listen(4000, ()=>{
+app.listen(process.env.PORT||5000, ()=>{
     console.log("Backend Server is running")
 })
